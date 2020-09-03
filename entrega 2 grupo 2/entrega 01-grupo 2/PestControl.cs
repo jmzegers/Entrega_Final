6d698bf742f03b9c87bf62f941ffa9faf7863f91
jmzegers <jmzegers@miuandes.cl>
2020-09-03 19:28:48 -0400

    Consumibles corregidos

--- entrega 2 grupo 2/entrega 01-grupo 2/PestControl.cs
+++ entrega 2 grupo 2/entrega 01-grupo 2/PestControl.cs
@@ -8,9 +8,33 @@ namespace entrega_01_grupo_2
 {
     class PestControl : Consumable
     {
-        public PestControl(int price, bool use, string enemy, double chanceOfSuccess) : base(price, use)
+        private string Enemy;
+        private double ChanceOfSuccess;
+        
+        public PestControl(int price, string enemy, double chanceOfSuccess) : base(price)
         {
+            this.Enemy = enemy;
+            this.ChanceOfSuccess = chanceOfSuccess;
+        }
 
+        public int GetPrice()
+        {
+            return Price;
+        }
+
+        public bool GetUse()
+        {
+            return Use;
+        }
+
+        public string GetEnemy()
+        {
+            return Enemy;
+        }
+
+        public double GetChanceOfSuccess()
+        {
+            return ChanceOfSuccess;
         }
     }
 }
--- entrega 2 grupo 2/entrega 01-grupo 2/Water.cs
+++ entrega 2 grupo 2/entrega 01-grupo 2/Water.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,33 @@ namespace entrega_01_grupo_2
 {
     class Water : Consumable
     {
+        private int Amount;
+        private string Target;
+
         public Water(int price, bool use, int amount, string target) : base(price, use)
         {
+            this.Amount = amount;
+            this.Target = target;
+        }
 
+        public int GetPrice()
+        {
+            return Price;
+        }
+
+        public bool GetUse()
+        {
+            return Use;
+        }
+
+        public int GetAmount()
+        {
+            return Amount;
+        }
+
+        public string GetTarget()
+        {
+            return Target;
         }
     }
 }
