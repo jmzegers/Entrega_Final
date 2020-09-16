72e00b82626a486f0257ca76f83a4331628df280
benjamin gonzalez <bigonzalez@miuandes.cl>
2020-09-16 18:22:11 -0300

    carpeta para tener ordenado las clases que se ocuparan en la enentrega 3

---
+++ entrega 2 grupo 2/entrega_3_prototipe/entrega_3_prototipe/Ir_al_mercado_de_consumibles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entrega_3_prototipe
+{
+    class Ir_al_mercado_de_consumibles
+    {
+    }
+}
---
+++ entrega 2 grupo 2/entrega_3_prototipe/entrega_3_prototipe/Revisar_los_precios_históricos_por_semilla.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entrega_3_prototipe
+{
+    class Revisar_los_precios_históricos_por_semilla
+    {
+    }
+}
