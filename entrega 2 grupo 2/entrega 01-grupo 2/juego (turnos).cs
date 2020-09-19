a79085db6858d449300c12961c7eaaee0e3e5531
benjamin gonzalez <bigonzalez@miuandes.cl>
2020-09-18 22:21:43 -0300

    mejora de la clase turnos

--- entrega 2 grupo 2/entrega 01-grupo 2/juego (turnos).cs
+++ entrega 2 grupo 2/entrega 01-grupo 2/juego (turnos).cs
@@ -8,5 +8,7 @@ namespace entrega_01_grupo_2
 {
     class tiempo__turnos_
     {
+        public delegate void ShiftTime(object source, EventArgs args);
+        public event ShiftTime PassTurn;
     }
 }
