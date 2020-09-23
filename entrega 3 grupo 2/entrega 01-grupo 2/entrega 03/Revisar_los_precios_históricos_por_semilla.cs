d7e78fc8a3c5fecc19fd4d069e6759007f7abb5c
benjamin gonzalez <bigonzalez@miuandes.cl>
2020-09-22 22:02:03 -0300

    a

--- entrega 3 grupo 2/entrega 01-grupo 2/entrega 03/Revisar_los_precios_históricos_por_semilla.cs
+++ entrega 3 grupo 2/entrega 01-grupo 2/entrega 03/Revisar_los_precios_históricos_por_semilla.cs
@@ -10,5 +10,38 @@ namespace entrega_01_grupo_2.entrega_03
     {
         public delegate void ReviewHistoricalPricesPerSeed(object source, EventArgs args);
         public event ReviewHistoricalPricesPerSeed HistoricalSeed;
+        Dictionary<string, Seed> seedDict = new Dictionary<string, Seed>();
+        Dictionary<string, Dictionary<int, double>> priceHistory = new Dictionary<string, Dictionary<int, double>>();
+        //Hay que crear varias semillas y dar sus valores
+        // Seed tomato = new Seed();
+        // Seed lettuce = new Seed();
+        // Seed carrot = new Seed();
+
+        Dictionary<string, Cattle> cattleDict = new Dictionary<string, Cattle>();
+        //Tambien hay que crear varios diferentes tipos de ganado con sus valores respectivos para rellenar este diccionario
+
+        Dictionary<string, StorageBuilding> storageBuildingDict = new Dictionary<string, StorageBuilding>();
+        //La misma historia
+
+        public Dictionary<string, Seed> GetSeedDict()
+        {
+            return seedDict;
+        }
+
+        public Dictionary<string, Dictionary<int, double>> GetPriceHistory()
+        {
+            return priceHistory;
+        }
+
+        public Dictionary<string, Cattle> GetCattleDict()
+        {
+            return cattleDict;
+        }
+
+        public Dictionary<string, StorageBuilding> GetStorageBuildingDict()
+        {
+            return storageBuildingDict;
+        }
     }
+
 }
